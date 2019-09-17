dc1d89703cf5a3511a9aa0cd5dc1390e97be9bc5
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 11:57:44 -0600

    Refactor PhotographTest to account with strings from csv data

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -10,17 +10,31 @@ class PhotographTest < Minitest::Test
       artist_id: "4",
       year: "1954"
     }
-    @photograph = Photograph.new(@attributes)
+
+    @attributes_with_strings = {
+      "id"=> "1",
+      "name"=> "Rue Mouffetard, Paris (Boy with Bottles)",
+      "artist_id"=> "4",
+      "year"=> "1954"
+    }
+    @photograph_1 = Photograph.new(@attributes)
+    @photograph_2 = Photograph.new(@attributes_with_strings)
   end
 
   def test_it_exists
-    assert_instance_of Photograph, @photograph
+    assert_instance_of Photograph, @photograph_1
+    assert_instance_of Photograph, @photograph_2
   end
 
   def test_initialize
-    assert_equal @attributes[:id], @photograph.id
-    assert_equal @attributes[:name], @photograph.name
-    assert_equal @attributes[:artist_id], @photograph.artist_id
-    assert_equal @attributes[:year], @photograph.year
+    assert_equal @attributes[:id], @photograph_1.id
+    assert_equal @attributes[:name], @photograph_1.name
+    assert_equal @attributes[:artist_id], @photograph_1.artist_id
+    assert_equal @attributes[:year], @photograph_1.year
+
+    assert_equal @attributes_with_strings["id"], @photograph_2.id
+    assert_equal @attributes_with_strings["name"], @photograph_2.name
+    assert_equal @attributes_with_strings["artist_id"], @photograph_2.artist_id
+    assert_equal @attributes_with_strings["year"], @photograph_2.year
   end
 end
