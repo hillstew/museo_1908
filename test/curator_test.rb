d1ae1eec66e9f114703c3c7aa5d1ffa6bb87ad5d
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 09:46:36 -0600

    Add test_it_can_add_photograph to CuratorTest

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -7,6 +7,18 @@ require './lib/curator'
 class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    @photo_2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+     })
   end
 
   def test_it_exists
@@ -15,5 +27,15 @@ class CuratorTest < Minitest::Test
 
   def test_initialize
     assert_equal [], @curator.photographs
+  end
+
+  def test_it_can_add_photograph
+    @curator.add_photograph(@photo_1)
+
+    assert_equal [@photo_1], @curator.photographs
+
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
   end
 end
