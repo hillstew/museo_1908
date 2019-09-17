2b63cd3b3de3a87f32cc528c67714fe49fa3b61f
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 11:44:56 -0600

    Add logic to Artist to handle data keys being strings

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,10 @@ class Artist
               :country
 
   def initialize(data)
-    @id = data[:id]
-    @name = data[:name]
-    @born = data[:born]
-    @died = data[:died]
-    @country = data[:country]
+    @id = data[:id] || data["id"]
+    @name = data[:name]|| data["name"]
+    @born = data[:born]|| data["born"]
+    @died = data[:died]|| data["died"]
+    @country = data[:country]|| data["country"]
   end
 end
