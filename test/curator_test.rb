6574eecf7321641244606a48c19b7b53704855fb
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 09:51:38 -0600

    Add test_it_can_add_an_artist to CuratorTest

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -19,6 +19,20 @@ class CuratorTest < Minitest::Test
       artist_id: "2",
       year: "1941"
      })
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    @artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
   end
 
   def test_it_exists
@@ -27,9 +41,10 @@ class CuratorTest < Minitest::Test
 
   def test_initialize
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
   end
 
-  def test_it_can_add_photograph
+  def test_it_can_add_a_photograph
     @curator.add_photograph(@photo_1)
 
     assert_equal [@photo_1], @curator.photographs
@@ -38,4 +53,16 @@ class CuratorTest < Minitest::Test
 
     assert_equal [@photo_1, @photo_2], @curator.photographs
   end
+
+  def test_it_can_add_an_artist
+    @curator.add_artist(@artist_1)
+
+    assert_equal [@artist_1], @curator.artists
+
+    @curator.add_artist(@artist_2)
+
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+
 end
