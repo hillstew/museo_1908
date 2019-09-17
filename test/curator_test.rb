ddcee49eb56f7a5f79f15ebeeeb37dff5898b0b0
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 09:30:07 -0600

    Add CuratorTest and test_it_exists

---
+++ test/curator_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require './lib/photograph'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+  def setup
+    @curator = Curator.new
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, @curator
+  end
+end
