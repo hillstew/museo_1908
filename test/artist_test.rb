1cbbb56b8a19c9e38337476a621a985ca82bd25f
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 09:25:47 -0600

    Add test_initialize in ArtistTest

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -17,4 +17,12 @@ class ArtistTest < Minitest::Test
   def test_it_exists
     assert_instance_of Artist, @artist
   end
+
+  def test_initialize
+    assert_equal @attributes[:id], @artist.id
+    assert_equal @attributes[:name], @artist.name
+    assert_equal @attributes[:born], @artist.born
+    assert_equal @attributes[:died], @artist.died
+    assert_equal @attributes[:country], @artist.country
+  end
 end
