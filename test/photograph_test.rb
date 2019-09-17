1cbf25c9a752c3b43170f4baf28deda3287c863b
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 09:15:57 -0600

    Add test_initialize for Photograph

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,16 +4,23 @@ require './lib/photograph'
 
 class PhotographTest < Minitest::Test
   def setup
-    attributes = {
+    @attributes = {
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
       artist_id: "4",
       year: "1954"
     }
-    @photograph = Photograph.new(attributes)
+    @photograph = Photograph.new(@attributes)
   end
 
   def test_it_exists
     assert_instance_of Photograph, @photograph
+  end
+
+  def test_initialize
+    assert_equal @attributes[:id], @photograph.id
+    assert_equal @attributes[:name], @photograph.name
+    assert_equal @attributes[:artist_id], @photograph.artist_id
+    assert_equal @attributes[:year], @photograph.year
   end
 end
